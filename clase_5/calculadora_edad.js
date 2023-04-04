e14f4de4356af3cb6aaaf40bb3a681cd4e2c744d
mundostr <idux.net@gmail.com>
2023-04-04 14:00:48 -0300

    Ejemplo de módulo externo (moment)

---
+++ clase_5/calculadora_edad.js
@@ -0,0 +1,23 @@
+/*
+Calculadora de edad utilizando módulo moment
+
+Proyecto nuevo: npm init -y
+Instalación módulo moment: npm install moment --save
+*/
+
+const moment = require('moment');
+
+const NACIMIENTO = '2022-03-30';
+
+// isValid retorna true o false, verifica si el formato del string de fecha utilizado es válido
+const fechaValida = moment(NACIMIENTO, true).isValid();
+
+if (fechaValida) {
+    const fechaNacimiento = moment(NACIMIENTO); // objeto de fecha moment utilizando el string de nacimiento
+    const fechaActual = moment(); // objeto de fecha moment sin argumentos, asigna la fecha actual
+    const diasTranscurridos = fechaActual.diff(fechaNacimiento, 'days');
+    
+    console.log(`Han pasado ${diasTranscurridos} días desde tu nacimiento`);
+} else {
+    console.log('La fecha de nacimiento ingresada no es válida');
+}
