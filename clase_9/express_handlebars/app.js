d9b5b6cbad8c0382da963ce32de2924d804029ee
mundostr <idux.net@gmail.com>
2023-04-18 21:06:01 -0300

    Ejemplo express con templates

--- clase_9/express_handlebars/app.js
+++ clase_9/express_handlebars/app.js
@@ -13,19 +13,12 @@ const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-// server.use('/api', router);
-// server.use('/public', express.static(`${__dirname}/public`));
+server.use('/api', router);
+server.use('/public', express.static(`${__dirname}/public`));
 
 server.engine('handlebars', engine());
 server.set('view engine', 'handlebars');
 server.set('views', './views');
-
-server.get('/', (req, res) => {
-    res.render('index', {
-        nombre: 'Carlos',
-        apellido: 'Perren'
-    });
-});
 
 server.listen(PUERTO, () => {
     console.log(`Servidor iniciado en puerto ${PUERTO}`);
--- clase_9/express_handlebars/routes/users.routes.js
+++ clase_9/express_handlebars/routes/users.routes.js
@@ -2,10 +2,20 @@ import { Router } from 'express';
 
 const router = Router();
 
-const users = [];
+const users = [
+    { firstName: 'Carlos', lastName: 'Perren', age: 48, email: 'idux.net@gmail.com', phone: '+5493492555555', rol: 'admin' },
+    { firstName: 'Juan', lastName: 'Perez', age: 40, email: 'jperez@gmail.com', phone: '+5493492555555', rol: 'user' },
+    { firstName: 'Pepe', lastName: 'Pompin', age: 12, email: 'ppompin@gmail.com', phone: '+5493492555556', rol: 'admin' }
+];
 
 router.get('/users', (req, res) => {
-    res.status(200).send(users);
+    // res.status(200).send(users);
+
+    const indiceRandom = Math.floor(Math.random() * users.length);
+    res.render('index', {
+        user: users[indiceRandom],
+        isAdmin: users[indiceRandom].rol === 'admin'
+    });
 });
 
 export default router;
