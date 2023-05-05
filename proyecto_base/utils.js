747c0ba124824f832c658787794569e38923de38
Carlos Perren <idux.net@gmail.com>
2023-05-05 16:00:46 -0300

    Borrado proyecto base

--- proyecto_base/utils.js
+++
@@ -1,6 +0,0 @@
-import * as url from 'url';
-
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
-export { __filename, __dirname };
