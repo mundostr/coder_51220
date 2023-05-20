408e5489a9dc6a4c0e5b13066fb70dbb5cd326db
Carlos Perren <idux.net@gmail.com>
2023-05-20 20:56:40 -0300

    Agregado sesiones y login

--- clase_18/src/api/users/users.routes.js
+++ clase_18/src/api/users/users.routes.js
@@ -12,4 +12,64 @@ userRouter.get('/users', async (req, res) => {
     }
 });
 
+userRouter.get('/setcookie', async (req, res) => {
+    try {
+        res.cookie('cookie1', 'Contenido1', { maxAge: 60000, signed: true });
+        res.status(200).send({ status: 'OK', data: 'Cookie1 generada' });
+    } catch (err) {
+        res.status(500).send({ status: 'ERR', error: err.message });
+    }
+});
+
+userRouter.get('/getcookies', async (req, res) => {
+    try {
+        // req.cookies = cookies comunes
+        // res.status(200).send({ status: 'OK', data: req.cookies });
+        // req.signedCookies = cookies firmadas
+        res.status(200).send({ status: 'OK', data: req.signedCookies });
+    } catch (err) {
+        res.status(500).send({ status: 'ERR', error: err.message });
+    }
+});
+
+userRouter.get('/deletecookies', async (req, res) => {
+    try {
+        res.clearCookie('cookie1');
+        res.status(200).send({ status: 'OK', data: 'Cookie1 borrada' });
+    } catch (err) {
+        res.status(500).send({ status: 'ERR', error: err.message });
+    }
+});
+
+userRouter.get('/login', async (req, res) => {
+    try {
+        let data;
+        if (req.session.counter) {
+            req.session.counter++;
+            data = `Este sitio ha sido visitado ${req.session.counter} veces`;
+        } else {
+            req.session.counter = 1;
+            req.session.userState = 'validated';
+            data = 'Bienvenido, gracias por su primer visita!';
+        }
+        res.status(200).send({ status: 'OK', data: data });
+    } catch (err) {
+        res.status(500).send({ status: 'ERR', error: err.message });
+    }
+});
+
+userRouter.get('/logout', async (req, res) => {
+    try {
+        req.session.destroy((err) => {
+            if (err) {
+                res.status(200).send({ status: 'OK', data: 'ERROR al cerrar sesión' });
+            } else {
+                res.status(200).send({ status: 'OK', data: 'Usuario deslogueado' });
+            }
+        });
+    } catch (err) {
+        res.status(500).send({ status: 'ERR', error: err.message });
+    }
+});
+
 export default userRouter;
--- clase_18/src/app.js
+++ clase_18/src/app.js
@@ -7,6 +7,8 @@ import {} from 'dotenv/config'
 import express from 'express';
 import mongoose from 'mongoose';
 import { engine } from 'express-handlebars';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 import userRouter from './api/users/users.routes.js';
 
@@ -16,6 +18,7 @@ import { __dirname } from './utils.js';
 // y utilizar la importación de dotenv config como primer línea arriba
 const PORT = parseInt(process.env.PORT) || 3000;
 const MONGOOSE_URL = process.env.MONGOOSE_URL;
+const COOKIE_SECRET = 'abcd1234';
 
 // Servidor Express y Socket.io compartiendo puerto
 const app = express();
@@ -23,6 +26,16 @@ const app = express();
 // Parseo correcto de urls
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Parseo de cookies
+app.use(cookieParser(COOKIE_SECRET));
+
+// Manejo de sesiones
+app.use(session({
+    secret: COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
 
 // Endpoints API REST
 app.use('/api', userRouter);
