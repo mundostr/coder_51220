e3f97d51adfb5673620c999b2fb0dc05dc1ad255
Carlos Perren <idux.net@gmail.com>
2023-07-11 21:31:11 -0300

    Ejemplos TDD implementación manual

---
+++ clase_31/tdd/login.js
@@ -0,0 +1,64 @@
+/**
+ * Generación de login mediante TDD
+ * Este es un segundo ejemplo manual
+ * 
+ * Qué debe retornar:
+ * T1: No se ha proporcionado un password si se recibe clave vacía
+ * T2: No se ha proporcionado un usuario si se recibe user vacío
+ * T3: Contraseña incorrecta si no coincide el password
+ * T4: Credenciales incorrectas si no coincide el user
+ * T5: Logueado si coinciden user y password
+ */
+
+const login = (user, pass) => {
+    if (pass == null || pass === '') return 'No se ha proporcionado contraseña'
+    if (user == null || user === '') return 'No se ha proporcionado usuario'
+    if (pass !== '123') return 'Contraseña incorrecta'
+    if (user !== 'coderUser') return 'Credenciales incorrectas'
+    return 'Logueado'
+}
+
+let test
+let passedTests = 0
+
+console.log('T1: No se ha proporcionado contraseña si pass está vacío.')
+if (login('cperren') === 'No se ha proporcionado contraseña') {
+    passedTests++
+    console.log('Passed')
+} else {
+    console.log('Failed')
+}
+
+console.log('T2: No se ha proporcionado usuario si user está vacío.')
+if (login('', 'abc123') === 'No se ha proporcionado usuario') {
+    passedTests++
+    console.log('Passed')
+} else {
+    console.log('Failed')
+}
+
+console.log('T3: Contraseña incorrecta si pass es incorrecto.')
+if (login('coderUser', 'abc123') === 'Contraseña incorrecta') {
+    passedTests++
+    console.log('Passed')
+} else {
+    console.log('Failed')
+}
+
+console.log('T4: Credenciales incorrectas si user es incorrecto.')
+if (login('cperren', '123') === 'Credenciales incorrectas') {
+    passedTests++
+    console.log('Passed')
+} else {
+    console.log('Failed')
+}
+
+console.log('T5: Logueado si user y pass coinciden.')
+if (login('coderUser', '123') === 'Logueado') {
+    passedTests++
+    console.log('Passed')
+} else {
+    console.log('Failed')
+}
+
+console.log(`Tests superados: ${passedTests} de 5`)
---
+++ clase_31/tdd/suma.js
@@ -0,0 +1,79 @@
+/**
+ * Primer ejemplo de desarrollo de funcionalidad por TDD
+ * TDD = Test Driven Development (Desarrollo basado en pruebas)
+ * En este primer ejemplo, plantearemos MANUALMENTE las pruebas
+ * 
+ * Paso 1: Enunciamos las pruebas que debe superar nuestra función suma(), comenzando con una función declarada vacía:
+ * Qué debe devolver:
+ * T1: 0 si no recibe parámetros
+ * T2: null si algún parámetro no es numérico
+ * T3: La suma de los parámetros
+ * T4: La suma de cualquier cantidad de parámetros
+ * 
+ * Por supuesto en esta etapa, TODAS las pruebas deben FALLAR.
+ * 
+ * Paso 2: Vamos implementando la función por etapas hasta lograr que TODAS las pruebas PASEN.
+ * 
+ * Paso 3: Refactorizamos, es decir, intentamos optimizar el código de la función, pero por supuesto
+ * TODAS las pruebas deben seguir PASANDO.
+ * 
+ * Repetimos la secuencia hasta que estemos conformes con la implementación de la función.
+ */
+
+const sumar = (...params) => {
+    if (params.length === 0) return 0
+    
+    // Estas dos líneas surgen de una refactorización y reemplazan al código comentado debajo
+    // permitiendo una función más compacta que sigue superando todos los tests planteados.
+    if (!params.every(num => typeof(num) === 'number')) return null
+    return params.reduce((accum, current) => accum + current, 0)
+    
+    // for (let i = 0; i < params.length; i++) { if (typeof(params[i]) !== 'number') return null }
+    
+    /* let suma = 0;
+    for (let i = 0; i < params.length; i++) {
+        suma += params[i]
+    }
+    return suma */
+}
+
+let test
+let passedTests = 0
+
+test = sumar(2, 3)
+console.log('T1: La suma de los parámetros')
+if (test === 5) {
+    passedTests++
+    console.log('Superado')
+} else {
+    console.log('Fallido')
+}
+
+test = sumar('2', 3)
+console.log('T2: null si algún parámetro no es numérico')
+if (test === null) {
+    passedTests++
+    console.log('Superado')
+} else {
+    console.log('Fallido')
+}
+
+test = sumar()
+console.log('T3: 0 si no recibe parámetros')
+if (test === 0) {
+    passedTests++
+    console.log('Superado')
+} else {
+    console.log('Fallido')
+}
+
+test = sumar(1, 2, 3, 4, 5)
+console.log('T4: La suma de cualquier cantidad de parámetros')
+if (test === 15) {
+    passedTests++
+    console.log('Superado')
+} else {
+    console.log('Fallido')
+}
+
+console.log(`Tests superados: ${passedTests} de 4`)
