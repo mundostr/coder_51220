0dbd5252834d80bd09cbcf2829b00b61a1ae0aa0
Carlos Perren <idux.net@gmail.com>
2023-08-03 16:37:28 -0300

    Ajuste comentario método uploadContent

--- utilidades/file_uploader/src/controllers/main.controller.js
+++ utilidades/file_uploader/src/controllers/main.controller.js
@@ -3,7 +3,7 @@ import imageModel from "../models/image.model.js";
 
 const uploadContent = async (req, res) => {
     // Si hay un file en el objeto request, quiere decir que se subió un archivo correctamente.
-    // Se lo lee desde disco, se lo codifica como cadeba base64 y se lo almacena en MongoDB.
+    // Se lo lee desde disco, se lo codifica como cadena base64 y se lo almacena en MongoDB.
     // Por supuesto podría borrarse luego el archivo de disco.
     if (req.file) {
         const file = fs.readFileSync(req.file.path);
