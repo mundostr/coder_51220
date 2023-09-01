e583e68d6424f90c3d6036b3b666876a6af9a569
Carlos Perren <idux.net@gmail.com>
2023-09-01 14:38:02 -0300

    Aclaración enlace Stripe

--- clase_46/stripe_payment/src/services/payment.service.js
+++ clase_46/stripe_payment/src/services/payment.service.js
@@ -1,11 +1,17 @@
 import Stripe from 'stripe'
 import { config } from '../config.js'
 
+// Una clase muy sencilla en la cual generamos una nueva instancia de Stripe
+// y le enviamos nuestra key para identificarnos y poder utilizar el servicio
 export default class PaymentService {
     constructor() {
         this.stripe = new Stripe(config.STRIPE_KEY)
     }
 
+    // Cada vez que llamemos a este método, le pediremos a Stripe que nos genere
+    // una nueva sesión de pagos. Tomará los datos que le enviemos y nos devolverá
+    // una URL a la cual podremos redireccionar para que allí el cliente vea los
+    // edtalles del pago y coloque los datos de su tarjeta.
     createPaymentSession = async (data) => {
         return this.stripe.checkout.sessions.create(data)
     }
