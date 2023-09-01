a7b1a58e1c2147828791021615247aa155938779
Carlos Perren <idux.net@gmail.com>
2023-09-01 14:35:44 -0300

    Mención rutas estáticas y checkout

--- clase_46/stripe_payment/src/routes/cart.routes.js
+++ clase_46/stripe_payment/src/routes/cart.routes.js
@@ -7,9 +7,13 @@ export const cartRoutes = ()  => {
     const router = Router();
     
     router.get('/', getProducts)
+    
+    // Estas dos rutas serán llamadas por el proceso de callback de Stripe,
+    // según como haya resultado el pago, mostrando la página estática correspondiente
     router.get('/success', (req, res) => { res.redirect('/success.html') })
     router.get('/cancel', (req, res) => { res.redirect('/cancel.html') })
     
+    // Esta ruta será llamada por el frontend al Comprar
     router.post('/checkout', checkout)
     
     return router
