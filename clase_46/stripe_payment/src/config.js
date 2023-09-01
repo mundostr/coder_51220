7d2707d12d5b985e2dd075ce52c407d101dab880
Carlos Perren <idux.net@gmail.com>
2023-09-01 14:30:17 -0300

    Mención path.resolve

--- clase_46/stripe_payment/src/config.js
+++ clase_46/stripe_payment/src/config.js
@@ -8,5 +8,7 @@ export const config = {
     APP_BASE: process.env.APP_BASE,
     MONGOOSE_URL: process.env.MONGOOSE_URL,
     STRIPE_KEY: process.env.STRIPE_KEY,
+    // Podemos también utilizar la alternativa de path.resolve() para obtener la ruta base de ejecución
+    // Esto será útil en la configuración de express.static() o handlebars
     APP_PATH: `${path.resolve()}/src`
 }
